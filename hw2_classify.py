ab094c1543dc187b491fea9e5657251c96c699ef
jwmatson <jwmatson@ualberta.ca>
2021-11-20 17:59:24 -0700

    documentation, evaluation.pdf

--- hw2_classify.py
+++ hw2_classify.py
@@ -1,62 +1,61 @@
 import math
 import os
+import sys
 
-def classifyEmails():
+#def classifyEmails()
+#opens the probability_ham/spam files
+#then proceeds to search through the test files and look for each unique word, and add its log10 probability of existing in the test file
+#after adding all the spam and ham values together, will write in the output(#).txt file wether it is spam or ham
+def classifyEmails(filesToUse):
     fileHam = open("D:/Programming/Repositories/SpamFiltering/spamFiltering/probability_ham_words.txt", "r").read().split()
     fileSpam = open("D:/Programming/Repositories/SpamFiltering/spamFiltering/probability_spam_words.txt", "r").read().split()
-
+    fileHam = fileHam[16:] # get rid of the initial line describing layout of file
+    fileSpam = fileSpam[16:]
     hamProbability = 0
-    spamProbability = 0 
-    inHamFile = False
-    inSpamFile = False
-    fileNo = 0
-    for file in os.listdir("D:/Programming/Repositories/SpamFiltering/spamFiltering/Data/Test/test_Lemmatized"):
-        if(file[1] == '.'):
-            fileNo = file[0]
-        else:
-            fileNo = file[0:2]
-        hamProbability = 0
-        spamProbability = 0
-        fileOpen = open("D:/Programming/Repositories/SpamFiltering/spamFiltering/Data/Test/test_Lemmatized/"+file)
-        fileWrite = open("D:/Programming/Repositories/SpamFiltering/spamFiltering/output"+str(fileNo)+".txt","w")
-        fileWrite.write("(1) P(Spam, all words)\n")
-        for line in fileOpen:
-            for word in line.split():
-                inSpamFile = False
-                if word in fileSpam:
-                  wordIndex = fileSpam.index(word)
-                  fileWrite.write("P('"+word+"'|Spam) = "+ str((fileSpam[wordIndex + 1] + "\n")))
-                  spamProbability += math.log10(float(fileSpam[wordIndex + 1]))
-                  inSpamFile = True
-                if inSpamFile == False:
-                    fileWrite.write("P('"+word+"'|Spam) = "+str(1/84722) +"\n")
-                    spamProbability += math.log10(1/84722)
-        fileWrite.write("log P(Spam, all words) = " + str(spamProbability) + "\n\n\n")
-        fileOpen.close()
+    spamProbability = 0
 
-        fileOpen = open("D:/Programming/Repositories/SpamFiltering/spamFiltering/Data/Test/test_Lemmatized/"+file) #open again so can iterate lines again
-        fileWrite.write("(2) P(Ham, all words)\n")
-        for line in fileOpen:
-            for word in line.split():
-                inHamFile = False
-                if word in fileHam:
-                    wordIndex = fileHam.index(word)
-                    fileWrite.write("P('"+word+"'|Ham) = "+ str((fileHam[wordIndex + 1] +"\n")))
-                    hamProbability += math.log10(float(fileHam[wordIndex + 1]))
-                    inHamFile = True 
-                if inHamFile == False:
-                    fileWrite.write("P('"+word+"'|Ham) = "+str(1/84722) + "\n")
-                    hamProbability += math.log10(1/84722)          
-        fileWrite.write("log P(Ham, all words) = " + str(hamProbability) + "\n\n\n")
-        fileWrite.write("\n")
-        fileWrite.write("Conclusion: This message is classified as")
+    fileOpen = open("D:/Programming/Repositories/SpamFiltering/spamFiltering/Data/Test/test_Lemmatized/"+filesToUse[1]) #will open test file
+    fileWrite = open("D:/Programming/Repositories/SpamFiltering/spamFiltering/"+filesToUse[2],"w") #open correct output file, will create if does not exist in directory
+    fileWrite.write("(1) P(Spam, all words)\n")
 
-        if(hamProbability > spamProbability):
-            print("HAM " + file)
-            fileWrite.write(" Ham\n")
-        else:
-            print("SPAM " + file)
-            fileWrite.write(" Spam\n")
+    for line in fileOpen:
+        for word in line.split(): #for each word in test file
+            inSpamFile = False 
+            if word in fileSpam: #if word is found in probability_spam_words.txt
+                wordIndex = fileSpam.index(word)
+                fileWrite.write("P('"+word+"'|Spam) = "+ str((fileSpam[wordIndex + 1] + "\n"))) #write probability value in
+                spamProbability += math.log10(float(fileSpam[wordIndex + 1])) #add log10 probability value to total
+                inSpamFile = True
+            if inSpamFile == False: #if word not found in probability_spam_words.txt
+                fileWrite.write("P('"+word+"'|Spam) = "+str(1/84722) +"\n")
+                spamProbability += math.log10(1/84722)
+    fileWrite.write("log P(Spam, all words) = " + str(round(spamProbability,8)) + "\n\n\n")
     fileOpen.close()
 
-classifyEmails()
+    fileOpen = open("D:/Programming/Repositories/SpamFiltering/spamFiltering/Data/Test/test_Lemmatized/"+filesToUse[1]) #open again so can iterate lines again
+    fileWrite.write("(2) P(Ham, all words)\n")
+    for line in fileOpen:
+        for word in line.split():
+            inHamFile = False
+            if word in fileHam: #if found in probability_ham_words.txt
+                wordIndex = fileHam.index(word)
+                fileWrite.write("P('"+word+"'|Ham) = "+ str((fileHam[wordIndex + 1] +"\n"))) #same thing, but with ham
+                hamProbability += math.log10(float(fileHam[wordIndex + 1])) 
+                inHamFile = True 
+            if inHamFile == False: #if word not found in probability_ham_words.txt
+                fileWrite.write("P('"+word+"'|Ham) = "+str(1/84722) + "\n")
+                hamProbability += math.log10(1/84722)          
+
+    fileWrite.write("log P(Ham, all words) = " + str(round(hamProbability,8)) + "\n\n\n")
+    fileWrite.write("\n")
+    fileWrite.write("Conclusion: This message is classified as")
+
+    if(hamProbability > spamProbability):
+        print("HAM " + filesToUse[1])
+        fileWrite.write(" Ham\n")
+    else:
+        print("SPAM " + filesToUse[1])
+        fileWrite.write(" Spam\n")
+    fileOpen.close()
+
+classifyEmails(sys.argv)
--- hw2_prob.py
+++ hw2_prob.py
@@ -1,6 +1,7 @@
 #Hw2_prob.py
 #Given test ham and spam files, will keep track of the occurance of each unique word, and determine their probability of occurance
 import os
+
 #def ProbWords()
 #Given spam and ham  test files, will record the number of occurances of each unique word
 def probWords():
@@ -13,7 +14,7 @@ def probWords():
             fileOpen = open("D:/Programming/Repositories/SpamFiltering/spamFiltering/Data/Train/train_Lemmatized/"+file,"r") 
             if(file[0] == '3' or file[0] == '5'): #ham dictionary
                 for line in fileOpen:
-                    for word in line.split():
+                    for word in line.split(): #for each word, increment its occurance in the appropriate dictionary
                         if(word not in hamWordOccurance):
                             hamWordOccurance[word] = 1
                             hamWordCount += 1
@@ -33,23 +34,25 @@ def probWords():
     writeProbabilityFiles(spamWordOccurance, spamWordCount, hamWordOccurance, hamWordCount)
 
 #def writeProbabilityFiles
-#Given the word 
+#Given the word count, will write in probability_ham_words.txt and probability_spam_words.txt the given probabiity of a word occuring 
+# by doing #Of times word is seen in all files / number of words in all
 def writeProbabilityFiles(spamWordOccurance, spamWordCount, hamWordOccurance, hamWordCount):
     fileHam = open("D:/Programming/Repositories/SpamFiltering/spamFiltering/probability_ham_words.txt", "w")
     fileSpam = open("D:/Programming/Repositories/SpamFiltering/spamFiltering/probability_spam_words.txt", "w")
-    print(spamWordCount + hamWordCount)
+    fileHam.write("***The probability of a word will be adjacent to the word, seperated by a single space***\n\n\n")
+    fileSpam.write("***The probability of a word will be adjacent to the word, seperated by a single space***\n\n\n")
     for word in spamWordOccurance:
         keyValue = spamWordOccurance.get(word)
         wordProbability = keyValue/spamWordCount
-        fileSpam.write(str(word) + " ")
-        fileSpam.write(str(wordProbability)+"\n")
+        fileSpam.write(str(word) + " ")             #write the word that is found in the test file
+        fileSpam.write(str(wordProbability)+"\n") #write the probability of occurance of said word
     fileSpam.close()
 
     
     for word in hamWordOccurance:
         keyValue = hamWordOccurance.get(word)
         wordProbability = keyValue/hamWordCount
-        fileHam.write(str(word) + " ")
-        fileHam.write(str(wordProbability)+"\n")
+        fileHam.write(str(word) + " ")           #write the word that is found in the test file
+        fileHam.write(str(wordProbability)+"\n") #write the probability of occurance of said word
     fileHam.close()
 probWords()
